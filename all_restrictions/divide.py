b7ea916731fafa0043cdf3dfcf239f86bd29cce9
Alex <samsonovaleksandra@gmail.com>
2020-11-02 16:14:37 +0300

    Restrictions final changes
    Deleted some useless files.

--- all_restrictions/divide.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('covid-stringency-index.csv', sep=';', encoding = 'utf8')
-print(df.head())
-_ = df.groupby(["Entity"]).apply(lambda x: x.to_csv(fr"Stringency{x.name}.csv", index=False, encoding="utf-8"))
